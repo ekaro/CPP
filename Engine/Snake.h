29034cef1441500b4ea35a5062ce08b1d1b5ad71
ekaro <marek.pekarovic@gmail.com>
2017-08-21 18:14:58 +0200

    Add files via upload

---
+++ Engine/Snake.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "Board.h"
+
+class Snake
+{
+private:
+	class Segment
+	{
+	public:
+		void InitHead(const Location& loc);
+		void InitBody();
+		void Follow(const Segment& next);
+		void MoveBy(const Location& delta_loc);
+		void Draw(Board& brd) const;
+	private:
+		Location loc;
+		Color c;
+	};
+public:
+	Snake(const Location& loc);
+	void MoveBy(const Location& delta_loc);
+	void Grow();
+	void Draw(Board& brd) const;
+private:
+	static constexpr Color headColor = Colors::Yellow;
+	static constexpr Color bodyColor = Colors::Green;
+	static constexpr int nSegmentsMax = 100;
+	Segment segments[nSegmentsMax];
+	int nSegments = 1;
+};
